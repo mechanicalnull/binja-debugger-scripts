919b889e6301c962751f1a27b7b3e661b804a115
mechanicalnull <mechanicalnull@gmail.com>
2020-04-28 08:21:15 -0700

    Naive implementation of differential debugging

---
+++ differential_debug.py
@@ -0,0 +1,117 @@
+
+#!/usr/bin/env python3
+# By mechanicalnull, for funsies
+
+"""
+Use differential debugging to find the first divergence between two runs.
+
+Written for well-behaved targets.
+"""
+
+import os
+import sys
+import time
+from typing import Iterable, List, Set
+
+from binaryninja.binaryview import BinaryViewType, BinaryView
+from binaryninja import core_version, core_ui_enabled, user_plugin_path
+
+# Add path for debugger import, needed in standalone headless execution
+if not core_ui_enabled():
+    sys.path.append(user_plugin_path())
+from debugger import DebugAdapter
+
+
+def get_debug_adapter() -> DebugAdapter.DebugAdapter:
+    """Helper to define type and encapsulate this in case of changes."""
+    return DebugAdapter.get_adapter_for_current_system()
+
+
+def get_pc(dbg: DebugAdapter.DebugAdapter) -> int:
+    pc_names = ['rip', 'eip', 'pc']
+    for reg_name in pc_names:
+        if reg_name in dbg.reg_list():
+            return dbg.reg_read(reg_name)
+    raise Exception('[!] No program counter name (%s) found in registers: %s' % (pc_names, dbg.reg_list()))
+
+
+def get_block_path(bv: BinaryView, target_file: str, args: List[str] = []) -> List[int]:
+    """Collect the list of basic blocks hit in order.
+
+    NOTE: this won't record coverage on addresses not defined as code and
+    software breakpoints can potentially cause issues if the target does
+    unfriendly things, like read it's own code, overlap blocks, or split
+    instructions.
+    """
+    dbg = get_debug_adapter()
+    dbg.exec(target_file, args)
+
+    breakpoints = set()
+    for block in bv.basic_blocks:
+        if block.start not in breakpoints:
+            dbg.breakpoint_set(block.start)
+            breakpoints.add(block.start)
+
+    block_path = []
+    last_breakpoint = None
+    while True:
+        (reason, _) = dbg.go()  # second item in tuple "data" unused
+        if reason == DebugAdapter.STOP_REASON.BREAKPOINT:
+            stop_addr = get_pc(dbg)
+            block_path.append(stop_addr)
+            if last_breakpoint:
+                dbg.breakpoint_set(last_breakpoint)  # TODO: figure out how to handle self-looping blocks 
+            dbg.breakpoint_clear(stop_addr)
+            last_breakpoint = stop_addr
+            #print('DBG: Hit 0x%x' % stop_addr)
+
+        elif reason == DebugAdapter.STOP_REASON.PROCESS_EXITED:
+            #print('[+] Target exited cleanly')
+            break
+        else:
+            raise Exception('[!] Unexpected stop reason: %s' % str(reason))
+
+    return block_path
+
+
+if __name__ == '__main__':
+
+    USAGE = '%s <target_file> [args1] -- [args2]' % sys.argv[0]
+    USAGE += '\nNOTE: the double dash is mandatory for standalone usage'
+    if len(sys.argv) < 3 or '--' not in sys.argv:
+        print(USAGE)
+        exit(0)
+
+    target_file = sys.argv[1]
+    remaining_args = sys.argv[2:]
+    doubledash_index = remaining_args.index('--')
+    args_1 = remaining_args[:doubledash_index]
+    args_2 = remaining_args[doubledash_index+1:]
+    print(f'args_1 {args_1}')
+    print(f'args_2 {args_2}')
+
+    print('[*] Loading BinaryView of %s' % target_file)
+    start_time = time.time()
+    bv = BinaryViewType.get_view_of_file(target_file)
+    bv.update_analysis_and_wait()
+    duration = time.time() - start_time
+    print('[*] Analysis finished in %.2f seconds\n' % duration)
+
+    start_time = time.time()
+    path_1 = get_block_path(bv, target_file, args_1)
+    duration = time.time() - start_time
+    print('[*] Path 1 (%d blocks) recorded in %.2f seconds' % (len(path_1), duration))
+
+    start_time = time.time()
+    path_2 = get_block_path(bv, target_file, args_2)
+    duration = time.time() - start_time
+    print('[*] Path 2 (%d blocks) recorded in %.2f seconds' % (len(path_2), duration))
+
+    for i in range(min(len(path_1), len(path_2))):
+        if path_1[i] != path_2[i]:
+            print('[+] First departure at %d blocks in:' % i)
+            print('    Path 1: 0x%x' % path_1[i])
+            print('    Path 2: 0x%x' % path_2[i])
+            print('    Shared predecessor block: 0x%x' % path_1[i-1])
+            break
+    print('[*] Done!')
